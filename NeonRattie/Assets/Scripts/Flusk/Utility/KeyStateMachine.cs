64c5801e33524a0d5faad68ecd9bdddf40600152
Flusk <tpflusk@gmail.com>
2017-04-18 02:33:50 +0200

    Statemachine boiler plate

---
+++ NeonRattie/Assets/Scripts/Flusk/Utility/KeyStateMachine.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Flusk.Utility
+{
+    public class KeyStateMachine<TKey, TState> : StateMachine<TState> where TState : IState
+    {
+        public Dictionary<TKey, TState> keyStates;
+        
+        public virtual void AddState(TKey key, TState state)
+        {
+            keyStates.Add(key, state);
+        }
+    }
+}
---
+++ NeonRattie/Assets/Scripts/Flusk/Utility/StateMachine.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace Flusk.Utility
+{
+    public class StateMachine<TState> where TState : IState
+    {
+        protected List<TState> states;
+        public IState CurrentState {get; protected set;}
+
+        public virtual void AddState (TState state)
+        {
+            states.Add(state);
+        }
+
+        public void Tick ()
+        {
+            if ( CurrentState == null )
+            {
+                return;
+            }
+            CurrentState.Tick();
+        }
+
+        public void ChangeState(IState state)
+        {
+            if ( CurrentState != null )
+            {
+                CurrentState.Exit(state);
+            }
+            CurrentState = state;
+        }
+    }
+
+}
