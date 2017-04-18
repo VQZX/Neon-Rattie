6f2a9327b52ad40c897f1a72ef906ea5e021a1c5
Flusk <tpflusk@gmail.com>
2017-04-18 02:36:18 +0200

    Rat control

---
+++ NeonRattie/Assets/Scripts/NeonRattie/Rat/RatController.cs
@@ -0,0 +1,17 @@
+﻿using Flusk.Utility;
+using UnityEngine;
+
+namespace NeonRattie.Rat
+{
+    public class RatController : MonoBehaviour
+    {
+        private StateMachine<RatStates> stateMachine = new StateMachine<RatStates>();
+
+        //states
+
+        private void Init()
+        {
+            //initialise state machine
+        }
+    }
+}
---
+++ NeonRattie/Assets/Scripts/NeonRattie/Rat/RatStates.cs
@@ -0,0 +1,22 @@
+﻿using Flusk.Utility;
+
+namespace NeonRattie.Rat
+{
+    public class RatStates : IState
+    {
+        public virtual void Enter(IState previousState)
+        {
+            
+        }
+
+        public virtual void Exit(IState nextState)
+        {
+            
+        }
+
+        public virtual void Tick()
+        {
+            
+        }
+    }
+}
